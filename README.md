ee619fa443b81e59f80bc0f6955139d826fec5c0
cooodledooodle <105159383+cooodledooodle@users.noreply.github.com>
2022-10-05 13:52:55 +0530

    add keyboard shortcut

--- README.md
+++ README.md
@@ -5,4 +5,8 @@ Clear elements for making selections i.e radio button &amp; checkboxes && input 
 [![name](https://github.com/shriaviator/jebah_chrome_extension/blob/main/images/WhatsApp%20Video%202022-04-04%20at%2008.41.16.gif)](#)
 
 
+### Keyboard Shortcut
+Clear all the input fields: `Alt + C`
 
+You can customise that from your browser, just go to
+`chrome://extensions/shortcuts`
--- background.js
+++ background.js
@@ -1,6 +1,22 @@
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
-    files: ['content.js']
+    func: clear,
   });
+});
+
+function clear() {
+  var elements = document.getElementsByTagName("input");
+  for (let i = 0; i < elements.length; i++) {
+    if (elements[i].type == "text" || elements[i].type == "number") {
+      elements[i].value = "";
+    } else if (elements[i].type == "radio" || elements[i].type == "checkbox") {
+      elements[i].checked = false;
+    }
+  }
+}
+
+
+chrome.commands.onCommand.addListener((command) => {
+
 });
